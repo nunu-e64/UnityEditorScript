6dc8bc69d80585c7e023c84d9bb3a00a72c5b9a9
Fumiya Amano <fumiya.amano@gree.co.jp>
2016-05-12 13:54:29 +0900

    add sukenchat

---
+++ SukenChat.cs
@@ -0,0 +1,19 @@
+﻿using UnityEngine;
+using UnityEditor;
+using System.Reflection;
+
+public class SukenChat : ScriptableObject {
+	static BindingFlags Flags = BindingFlags.Public | BindingFlags.Static;
+
+	[MenuItem ("Tool/SukenChat")]
+	static void Open () {
+		var type = Types.GetType ("UnityEditor.Web.WebViewEditorWindow", "UnityEditor.dll");
+		var methodInfo = type.GetMethod ("Create", Flags);
+		methodInfo = methodInfo.MakeGenericMethod (typeof (SukenChat));
+		methodInfo.Invoke (null, new object[]{
+            "Suken_Chat",
+            "http://chat.kanichat.com/chat?roomid=SukenChat72",
+            100, 100, 200, 200
+        });
+	}
+}
