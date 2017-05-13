646f8f168ed4f77f3f7044763060a6c98e587fd6
Fumiya Amano <fumiya.amano@gree.co.jp>
2017-05-13 22:43:45 +0900

    fix build error

--- SukenChat.cs
+++ SukenChat.cs
@@ -2,18 +2,21 @@
 using UnityEditor;
 using System.Reflection;
 
-public class SukenChat : ScriptableObject {
+public class SukenChat : ScriptableObject
+{
 	static BindingFlags Flags = BindingFlags.Public | BindingFlags.Static;
 
-	[MenuItem ("Tool/SukenChat")]
-	static void Open () {
-		var type = Types.GetType ("UnityEditor.Web.WebViewEditorWindow", "UnityEditor.dll");
-		var methodInfo = type.GetMethod ("Create", Flags);
-		methodInfo = methodInfo.MakeGenericMethod (typeof (SukenChat));
-		methodInfo.Invoke (null, new object[]{
-            "Suken_Chat",
-            "http://chat.kanichat.com/chat?roomid=SukenChat72",
-            100, 100, 200, 200
-        });
+	[MenuItem("Tool/SukenChat")]
+	static void Open()
+	{
+		string typeName = "UnityEditor.Web.WebViewEditorWindow";
+		var type = System.Reflection.Assembly.Load("UnityEngine.dll").GetType(typeName);
+		var methodInfo = type.GetMethod("Create", Flags);
+		methodInfo = methodInfo.MakeGenericMethod(typeof(SukenChat));
+		methodInfo.Invoke(null, new object[] {
+			"Suken_Chat",
+			"http://chat.kanichat.com/chat?roomid=SukenChat72",
+			100, 100, 200, 200
+		});
 	}
 }
